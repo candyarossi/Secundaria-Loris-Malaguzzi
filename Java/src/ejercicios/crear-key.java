be5c5a52a218ba07a219b424e9360ac001573276
Candela Yarossi <candyarossi@gmail.com>
2021-08-22 16:23:59 -0300

    22-08-21

--- Java/src/ejercicios/crear-key.java
+++ Java/src/ejercicios/crear-key.java
@@ -2,19 +2,13 @@ class CrearKey
  {
    public static void imprimirKey()
    { 
-       
-     String key1 = "";
-       
-     String key2 = generarKey(key1);
-     
-     System.out.println(key2);
+     String key = generarKey();
+     System.out.println("Key: " + key);
    }
    
    public static char generarChar(){
-     
      int numero = (int)(Math.random()*90+1);
      char letra = 'a';
-     
      if(numero >= 48 && numero <= 57){
          letra = (char)numero;
      }else if(numero >= 65 && numero <= 90){
@@ -22,14 +16,11 @@ class CrearKey
      }else if(numero == 48 || numero == 49 || numero == 65 ){
          letra = 'a';
      }
-
      return letra;
    }
    
-   public static String generarKey(String str){
-     
-     StringBuilder strbuilder = new StringBuilder(str);
-     
+   public static String generarKey(){
+     StringBuilder strbuilder = new StringBuilder();
      while(strbuilder.length() < 25){
          char letra = generarChar();
          
@@ -37,11 +28,9 @@ class CrearKey
            strbuilder.append(letra);
          }
      }
-     
      for(int i=5; i<25; i=i+6){
          strbuilder.insert(i, "-");
      }
-       
      return strbuilder.toString();
    }
  }
---
+++ Lua/src/ejercicios/tpe2.lua
@@ -0,0 +1,130 @@
+--      Trabajo Práctico Evaluativo N°2:  Lenguaje Lua
+--      Resolución:
+
+local lista = {}
+
+function MostrarLista(lista)
+    print("VALORES DE LA LISTA:")
+    for i=1, #lista, 1 do
+        print(lista[i])
+    end
+end
+
+--1.	Hacer una función que permita ingresar varios elementos a una lista, tantos como quiera el usuario.
+
+function IngresarElementosALista()
+    local rta = "si"
+    repeat
+        print("Desea ingresar un nuevo elemento a la lista? si/no")
+        rta = io.read("*line")
+        if rta == "si" then
+            print("Ingrese un numero: ")
+            local nro = io.read("*n")
+            table.insert(lista, nro)
+        end
+    until rta == "no"
+end
+
+IngresarElementosALista()
+MostrarLista(lista)
+
+--2.	Hacer una función que pase todos los elementos de una lista a otra.
+
+local lista2 = {}
+
+function PasarDeUnaListaAOtra(lista1, lista2)
+    for i=1, #lista1, 1 do
+        table.insert(lista2, lista1[i])
+    end
+end
+
+PasarDeUnaListaAOtra(lista, lista2)
+print("Lista 1:")
+MostrarLista(lista)
+print("Lista 2:")
+MostrarLista(lista2)
+
+--3.	Hacer una función que encuentre el menor elemento de una lista y lo retorne.
+
+function DevolverMenorDeUnaLista(lista)
+    local menor = lista[1]
+    for i=1, #lista, 1 do
+        if menor > lista[i] then
+            menor = lista[i]
+        end
+    end
+    return menor
+end
+
+print("Menor elemento de la lista: ", DevolverMenorDeUnaLista(lista))
+
+--4.	Hacer una función que inserte en una lista ordenada un nuevo elemento, conservando el orden de esta.
+
+table.sort(lista)
+print("Lista anterior ordenada:")
+MostrarLista(lista)
+local nuevo = 4
+
+function InsertarElementoYOrdenar(lista, nuevo)
+    table.insert(lista, nuevo)
+    table.sort(lista)
+end
+
+InsertarElementoYOrdenar(lista, nuevo)
+print("Elemento insertado: ", nuevo)
+print("Lista nueva ordenada:")
+MostrarLista(lista)
+
+--5.	Hacer una función que sume todos los elementos de una lista, sin alterar su contenido, y retorne su resultado.
+
+function SumarElementosDeUnaLista(lista)
+    local resultado = 0
+    for i=1, #lista, 1 do
+        resultado = resultado + lista[i]
+    end
+    return resultado
+end
+ 
+print("Suma de todos los elementos de la lista: ", SumarElementosDeUnaLista(lista))
+
+--6.	Hacer una función que calcule el promedio de los elementos de una lista, para ello se puede usar la función del punto 5 y agregar la división por la cantidad de elementos de la lista.
+
+-- V1
+function PromedioDeLosElementosDeUnaLista(lista)
+    local suma = SumarElementosDeUnaLista(lista)
+    local promedio = suma / #lista
+    return promedio
+end
+
+-- V2
+function PromedioDeLosElementosDeUnaLista(lista)
+    local promedio = SumarElementosDeUnaLista(lista) / #lista
+    return promedio
+end
+
+-- V3
+function PromedioDeLosElementosDeUnaLista(lista)
+    return SumarElementosDeUnaLista(lista) / #lista
+end
+
+print("Promedio de todos los elementos de la lista: ", PromedioDeLosElementosDeUnaLista(lista))
+
+--7.	Hacer una función que reciba una lista con números de un solo dígito y que transforme esos dígitos en un número decimal. Por ejemplo, la lista:
+--      local lista = {1,5,9,3,6,8}
+--      Resultado de la función: 159368
+
+local listaDada = {1,5,9,3,6,8}
+
+-- V1
+function TransformarListaANumeroDecimal(lista)
+    local nro = table.concat(lista, "")
+    return nro
+end
+
+-- V2
+function TransformarListaANumeroDecimal(lista)
+    return table.concat(lista, "")
+end
+
+print("Numero decimal: ", TransformarListaANumeroDecimal(listaDada))
+MostrarLista(listaDada)
