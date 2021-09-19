a832a22098b0c1cafe17a3e83d373d2264e4f438
Candela Yarossi <candyarossi@gmail.com>
2021-09-19 19:01:22 -0300

    19-09-21

---
+++ Lua/src/ejercicios/pong/demo.lua
@@ -0,0 +1,73 @@
+-- REPASO Y DEMAS
+--
+-- Variables
+--
+
+-- global (accesible desde todo el programa Lua)
+hello = 'Hello'
+
+-- local (accesible sólo dentro del archivo/función)
+local world = ' World!'
+
+--
+-- Funciones
+--
+
+-- Declaración de una función
+function say(text)
+    print(text)
+end
+
+-- Llamado a una función
+say("Hello world!")
+
+--
+-- Sentencia IF
+--
+if world == 'World' then
+    print('World!')
+else
+    print('Hello!')
+end
+
+--
+-- Bucles
+--
+
+-- Bucle WHILE con un contador
+local i = 10
+while i > 0 do
+    i = i - 1
+    print(i)
+end
+
+-- Un bucle FOR que decrementa desde el 10 hasta el 1
+for j = 10, 1, -1 do
+    print(j)
+end
+
+-- Bucle REPEAT
+i = 10
+repeat
+    i = i - 1
+    print(i)
+until i == 0
+
+--
+-- Tablas
+--
+
+-- Creación de una lista de clave-valor
+local person = {}
+person.name = 'Candela'
+person.age = 24
+person.height = 1.55
+
+-- Formas de acceder a cada valor
+print(person['name'])
+print(person.name)
+
+-- Iteración de a pares(clave-valor) con la muestra de ambos valores
+for key, value in pairs(person) do
+    print(key, value)
+end
---
+++ Lua/src/ejercicios/pong/pong-0/main.lua
@@ -0,0 +1,52 @@
+--[[
+    PONG - TP Final: Lenguaje Lua & Love2d - 6to año - Instituto Loris Malaguzzi
+    Traducción: Candela Yarossi
+    candyarossi@gmail.com
+
+    -- PONG 0 - Momento 0 --
+
+    Fuente Original: GD50 2020 - Pong remake
+    Autor: Colton Ogden
+    cogden@cs50.harvard.edu 
+
+
+    -- Programa principal --
+
+    Programado originalmente por Atari en 1972. Cuenta con dos
+    paletas, controladas por dos jugadores, con el objetivo de conseguir
+    llevar la pelota más allá del límite de su oponente. El primero en 
+    conseguir 10 puntos gana
+
+    Esta versión está diseñada para parecerse más a la NES que a
+    las máquinas Pong originales o el Atari 2600 en términos de
+    resolución, aunque en pantalla ancha (16:9), por lo que se ve 
+    mejor en sistemas modernos
+]]
+
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+--[[
+    love.load() Se ejecuta cuando el juego se inicia por primera vez, solo una vez; 
+    utilizado para inicializar el juego
+]]
+function love.load()
+    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
+        fullscreen = false,
+        resizable = false,
+        vsync = true
+    })
+end
+
+--[[
+    love.draw() Llamado después de la actualización (update) por LÖVE2D, se usa para 
+    dibujar cualquier cosa en la pantalla, actualizado o no
+]]
+function love.draw()
+    love.graphics.printf(
+        'Hello Pong!',          -- Texto a renderizar
+        0,                      -- Inicio de X (0 ya que lo vamos a centrar en función del ancho)
+        WINDOW_HEIGHT / 2 - 6,  -- Inicio de Y (a la mitad de la pantalla) (6px aprox. de tamaño de fuente)
+        WINDOW_WIDTH,           -- Número de píxeles para centrar el texto dentro (toda la pantalla aquí)
+        'center')               -- Modo de alineación, puede ser 'center', 'left', o 'right'
+end
